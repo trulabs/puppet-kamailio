432256f3e597c19b0ff57aa7b2d1cd3a9aa36f3f
Giacomo Vacca <giacomo.vacca@gmail.com>
2014-03-27 12:39:05 +0000

    Add a sort of node template in test environment

--- environments/test/manifests/nodes/kamailio_nodes.pp
+++ environments/test/manifests/nodes/kamailio_nodes.pp
@@ -1,5 +1,15 @@
 #nodes.pp
 
+node 'YOUR HOST NAME HERE' {
+
+  class { '::kamailio':
+    service_manage => true,
+    service_enable => true,
+    service_ensure => 'running',
+    manage_repo    => true,
+  }
+}
+
 node 'debian7' {
 
   class { '::kamailio':
