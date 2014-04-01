0042e6bdd1259e8aa00883d717747d5ea96fcff2
Giacomo Vacca <giacomo.vacca@gmail.com>
2014-04-01 18:09:49 +0100

    Added documentation for kamailio class parameters

--- modules/kamailio/manifests/init.pp
+++ modules/kamailio/manifests/init.pp
@@ -23,6 +23,34 @@
 # Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
 # Neither the name of Truphone nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# 
+# === Parameters
+#
+# [*service_enable*]
+#   Boolean.
+#   Default: false.
+#
+# [*service_ensure*]
+#   String. Ensure the service is running or stopped.
+#   Options: running, stopped
+#   Default: running
+# 
+# [*service_manage*]
+#   Boolean. Whether Puppet should manage this service.
+#   Default: false
+# 
+# [*manage_repo*]
+#   Boolean. Whether Puppet should manage the package repo.
+#   Default: false
+# 
+# [*package_ensure*]
+#   String. Ensure the package is present or not.
+#   Options: present, latest, absent
+#   Default: present
+# 
+# [*package_name*]
+#   String. What package name to use for kamailio.
+#   Default: kamailio
 
 class kamailio(
   $service_enable = $kamailio::params::service_enable,
