2af8934722dbe7549b64c0ad8ea37cd4c6b84949
Giacomo Vacca <giacomo.vacca@gmail.com>
2014-04-29 17:36:45 +0000

    Node definition updated with usage of the new parameters (manage_config and with_websocket)

--- environments/test/manifests/nodes/kamailio_nodes.pp
+++ environments/test/manifests/nodes/kamailio_nodes.pp
@@ -3,20 +3,25 @@
 node 'YOURHOSTNAMEHERE' {
 
   class { '::kamailio':
-    service_manage => true,
-    service_enable => true,
-    service_ensure => 'running',
-    manage_repo    => true,
+    service_manage  => true,
+    service_enable  => true,
+    service_ensure  => 'running',
+    manage_repo     => true,
+    with_tls        => true,
+    with_websockets => true,
+    manage_config   => true,
   }
 }
 
 node 'debian7' {
 
   class { '::kamailio':
-    service_manage => true,
-    service_enable => true,
-    service_ensure => 'running',
-    manage_repo    => true,
-    with_tls       => true,
+    service_manage  => true,
+    service_enable  => true,
+    service_ensure  => 'running',
+    manage_repo     => true,
+    with_tls        => true,
+    with_websockets => true,
+    manage_config   => true,
   }
 }
