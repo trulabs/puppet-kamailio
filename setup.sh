ea43045fe4a2f675e50d10a826a6de34769179d2
Giacomo Vacca <giacomo.vacca@gmail.com>
2014-10-08 15:48:23 +0000

    No need for setup.sh now

--- setup.sh
+++
@@ -1,6 +0,0 @@
-apt-get update && apt-get install -y puppet
-
-## This will also pull puppetlabs-apt and puppetlabs-stdlib
-puppet module install trulabs-kamailio
-
-puppet apply -v /etc/puppet/modules/kamailio/tests/init.pp --show_diff --noop
