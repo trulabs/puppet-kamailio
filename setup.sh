68c5bc4c3c03813e134c92d988a8faab6a74e8e4
Giacomo Vacca <giacomo.vacca@gmail.com>
2014-10-08 16:20:36 +0100

    Fixed typo.

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@ apt-get update && apt-get install -y puppet
 ## This will also pull puppetlabs-apt and puppetlabs-stdlib
 puppet module install trulabs-kamailio
 
-puppet apply -v /etc/puppet/modules/asterisk/tests/init.pp --show_diff --noop
+puppet apply -v /etc/puppet/modules/kamailio/tests/init.pp --show_diff --noop
